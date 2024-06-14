539b66e1c5c0ea3a0039f09ef856fa6a9ff4d1b9
Kelly Appleton <kda9036@rit.edu>
2024-06-13 22:19:26 -0400

    use key value pairs for sorting option render

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,23 @@
 import styles from "./SearchBar.module.css";
 
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
+};
+
 export default function SearchBar() {
+  const renderSortByOption = () => {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  };
+
   return (
     <div className={styles.SearchBar}>
       <div className={styles.SearchBarSortOptions}>
-        <ul>
-          <li>Best Match</li>
-          <li>Highest Rated</li>
-          <li>Most Reviewed</li>
-        </ul>
+        <ul>{renderSortByOption()}</ul>
       </div>
       <div className={styles.SearchBarFields}>
         <input type="text" name="businesses" placeholder="Search Businesses" />

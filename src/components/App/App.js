293698719afce2a04b5a53906ed6f629b94300e3
Kelly Appleton <kda9036@rit.edu>
2024-06-13 21:47:31 -0400

    added business component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
+import Business from "../Business/Business";
 
 function App() {
-  return <div className={styles.App}>Placeholder</div>;
+  return (
+    <div className={styles.App}>
+      <Business />
+    </div>
+  );
 }
 
 export default App;
